978da5d65d36e63faafd270ee7a9dc22099c5d7b
humzaahmad906 <humzaahmad9066@gmail.com>
2021-11-30 23:33:19 +0500

    add text box and svg on fabric canvas

--- src/Components/UI/recommendation_form/recommendationform.js
+++ src/Components/UI/recommendation_form/recommendationform.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import {fabric} from "fabric";
 import {FormControl, InputLabel, Input, Button, Typography} from '@material-ui/core';
+import FontFaceObserver from "fontfaceobserver"
 
 
 class RecommendationForm extends React.Component{
@@ -27,7 +29,23 @@ class RecommendationForm extends React.Component{
         this.setState({data: category})
 
     }
+    getRandomElement = (array) => (
+        array[Math.floor(Math.random() * array.length)]
+    )
+    loadFont = (font) => {
+        let myfont = new FontFaceObserver(font)
+        myfont.load()
+            .then(() => {
+                // when font is loaded, use it.
+                this.canvas.getActiveObject().set("fontFamily", font);
+                this.canvas.renderAll();
+            }).catch((e) => {
+            console.log(e)
+            alert('font loading failed ' + font);
+        });
+    }
     componentDidMount(){
+        this.canvas = new fabric.Canvas("c");
         axios.get('http://127.0.0.1:5000/get_categories')
         .then( (response) => {
             const categories = response["data"]
@@ -47,15 +65,104 @@ class RecommendationForm extends React.Component{
             });
     }
 
-    onSubmit = (value) => {
-        console.log(value)
+//     var canvas = new fabric.Canvas('c');
+// // Define an array with all fonts
+//     var fonts = ["Pacifico", "VT323", "Quicksand", "Inconsolata"];
+//
+//     var textbox = new fabric.Textbox('Lorum ipsum dolor sit amet', {
+//         left: 50,
+//         top: 50,
+//         width: 150,
+//         fontSize: 20
+//     });
+//     canvas.add(textbox).setActiveObject(textbox);
+//     fonts.unshift('Times New Roman');
+// // Populate the fontFamily select
+//     var select = document.getElementById("font-family");
+//     fonts.forEach(function(font) {
+//         var option = document.createElement('option');
+//         option.innerHTML = font;
+//         option.value = font;
+//         select.appendChild(option);
+//     });
+//
+// // Apply selected font on change
+//     document.getElementById('font-family').onchange = function() {
+//         if (this.value !== 'Times New Roman') {
+//             loadAndUse(this.value);
+//         } else {
+//             canvas.getActiveObject().set("fontFamily", this.value);
+//             canvas.requestRenderAll();
+//         }
+//     };
+//
+//     function loadAndUse(font) {
+//         var myfont = new FontFaceObserver(font)
+//         myfont.load()
+//             .then(function() {
+//                 // when font is loaded, use it.
+//                 canvas.getActiveObject().set("fontFamily", font);
+//                 canvas.requestRenderAll();
+//             }).catch(function(e) {
+//             console.log(e)
+//             alert('font loading failed ' + font);
+//         });
+//     }
+
+    onSubmit = (item) => {
+
+        axios.post("http://127.0.0.1:5000/get_svg", {category: item.category}).then((response)=>{
+            let svg_img = new String(response.data)
+            console.log(svg_img)
+            this.canvas.backgroundColor = 'rgb(150,150,150)';
+            let path = fabric.loadSVGFromString(svg_img, (objects, options)=>{
+                let obj = fabric.util.groupSVGElements(objects, options);
+
+                this.canvas.add(obj);
+                this.canvas.renderAll();
+                const object = this.canvas.getObjects()[this.canvas.getObjects().length-1]
+                this.canvas.setActiveObject(object)
+                const activeObject = this.canvas.getActiveObject()
+
+                const boundingRect = activeObject.getBoundingRect()
+                let scale = 1
+                if(boundingRect.width>boundingRect.height){
+                    scale = 100/activeObject.width;
+                }else{
+                    scale = 100/activeObject.height;
+                }
+                console.log(scale*activeObject.height, scale*activeObject.width)
+                activeObject.scale(scale)
+                activeObject.set({originX: 'center',
+                                originY: 'center'})
+                activeObject.set({left: this.canvas.getWidth()/2, top: this.canvas.getHeight()/2})
+                activeObject.setCoords()
+                const fonts = ["Pacifico", "VT323", "Quicksand", "Inconsolata"];
+                const margin = 2
+                let text = new fabric.Textbox(this.state.companyName, {
+                    fontSize: 16,
+                    textAlign: 'center',
+                    originX: "center",
+                    originY: "center",
+                    left: this.canvas.getWidth()/2,
+                    top: this.canvas.getHeight()/2-scale*activeObject.height-margin,
+                });
+                this.canvas.add(text)
+                let text_object = this.canvas.getObjects()[this.canvas.getObjects().length-1]
+                this.canvas.setActiveObject(text_object)
+                let fontType = this.getRandomElement(fonts)
+                this.loadFont(fontType)
+                this.canvas.renderAll()
+                this.setState({data: []})
+            })
+        })
     }
 
     render(){
         return (
-            <div className={"d-flex flex-wrap"}>
+            <div className={"d-flex flex-wrap justify-content-between"}>
+                <canvas id="c" width={"1000px"} height={"1000px"}></canvas>
                 <FormControl>
-
                     <Typography>Company Name</Typography>
                     <Input id="my-input"
                     value={this.state.companyName} 
@@ -70,7 +177,6 @@ class RecommendationForm extends React.Component{
                     </ul>
 
                     <Typography>Recommended Colors</Typography>
-
                     <div className={"d-flex flex-wrap justify-content-center"} style={{'width':"300px"}}>
                         {
                             this.state.recommendedColors.map((color, id)=><div
@@ -87,12 +193,13 @@ class RecommendationForm extends React.Component{
                     fullWidth={false}
                     variant="outlined"
                     color="primary"
-                    onClick={() => this.onSubmit(this.state.companyName)}
+                    onClick={() => this.onSubmit(this.state.data.filter(item=>item.selected)[0])}
                     >
                         Submit
                     </Button>
                 </FormControl>
                 <div className={"d-flex flex-wrap align-self-start"} style={{'width':"300px"}}>
+                    <Typography>Selected Colors</Typography>
                     {
                         this.state.recommendedColors.map((color, id)=> {
                             if(color.selected){
